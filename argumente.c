1cfccc3681ea8099f1b0e48c7b5f5715dfa8d4e0
JanMeyfarth1999 <jan_meyfarth@teilnehmer.cdemy.de>
2026-08-20 09:12:03 +0200

    aufgabe 1 & 2

---
+++ argumente.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+
+    printf("Anzahl der Argumente: %d\n", argc);
+
+    return 0;
+}
---
+++ bestimmtesargument.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+
+    printf("Das bestimmte Argument ist %s", argv[1]);
+
+    return 0;
+}
